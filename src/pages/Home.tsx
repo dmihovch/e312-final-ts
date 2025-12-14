e4d9c562e9476e14623f232cbecaa96e94e89596
Dan Mihovch <dmihovch@gmail.com>
2025-12-13 19:44:42 -0500

    prog

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,54 @@
+import "../styles/home.css";
+
 export default function Home() {
-	return <p>Home</p>;
+	return (
+		<div id="page">
+			<div id="home-page-wrapper">
+				<div id="left-column">
+					<div id="name-box">
+						<div id="name-row">
+							<h1 id="first-name">Dan</h1>
+							<p id="major">
+								Junior Computer
+								<br />
+								Science Major @ UD
+							</p>
+						</div>
+						<h1 id="last-name">Mihovch</h1>
+					</div>
+
+					<div id="pic-text-wrap">
+						<div id="leia">
+							<img src="src/assets/leia-pic.jpg" alt="Leia" />
+						</div>
+						<div id="leia-text">
+							<p>
+								I've been told my cat is easier on the eyes than myself, so she
+								will be representing my likeness hereinafter
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div id="about">
+					<h3>Howdy!</h3>
+					<p>
+						As the big bold letters have told you already, my name is Dan
+						Mihovch, and I am a junior Computer Science Major at the University
+						of Delaware. What the big bold letters didn't tell you, is that
+						askdjsal;dkfjasldkfjhaslkdjfhalskdjfhasldkjfhasldkfj hasldkfj
+						hasldkfj hasldkfjh asldkfjhas ldkfjhas ldkfjhas dlfkjhas dlkfjhas
+						dlkfjhas dlkfjhasdlfkjhas dlfkjhasdlfk jahsdflkjahsd flkjahsdf
+						lkajshdf laksjdhf alskjdfh alskdjfh asldkjfh asldkfhaslkdfjhas
+						ldkfjh as dlkfj ahsdl fkjashd lfkjashdf lkajsdhf laskjdhf alskdjf
+						alskdfjh aslkdf:w
+					</p>
+				</div>
+			</div>
+
+			<div id="footer-wrapper">
+				<p id="footer">University of Delaware, English 312 Final</p>
+			</div>
+		</div>
+	);
 }
